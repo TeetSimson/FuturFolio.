abac2f6b63cab390ca4aca70390176edd5b98b97
a58946lt <lu.tian-2@student.manchester.ac.uk>
2021-02-21 21:04:21 +0000

    Added the basics of a dropdown menu

---
+++ FuturFolio_FrontEnd/futurfolio/src/Components/Topbar/Icons/readme.txt
@@ -0,0 +1,18 @@
+Credits:
+
+Creative Commons(Attribution 3.0 unported):
+profileIcon.svg - profile icon, made by icon king1, https://freeicons.io/common-style-icons-11/profile-icon-12054
+
+bellIcon.svg - essentials, notification, ring, bell, alarm icon, icon by "https://freeicons.io/profile/2257",www.wishforge.games on https://freeicons.io"   
+
+Creative Commons(Attribution 3.0 unported):
+cogIcon.svg - Icon by Raj Dev on freeicons.io
+
+chevronRight.svg - Icon by Raj Dev on freeicons.io
+
+powerIcon.svg - Icon by Fasil on freeicons.io
+                                
+
+
+                                
+                             
--- FuturFolio_FrontEnd/futurfolio/src/Components/Topbar/Topbar.js
+++ FuturFolio_FrontEnd/futurfolio/src/Components/Topbar/Topbar.js
@@ -1,23 +1,75 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import '../../App.css';
 import "./Topbar.css";
 import profilepic from './profilepic.jpg';
+import { ReactComponent as ProfileIcon } from './Icons/profileIcon.svg';
+import { ReactComponent as BellIcon } from './Icons/bellIcon.svg';
+import { ReactComponent as CogIcon } from './Icons/cogIcon.svg';
+import { ReactComponent as RightChevron } from './Icons/chevronRight.svg';
+import { ReactComponent as PowerIcon } from './Icons/powerIcon.svg';
+
+
 
 
 export default class Topbar extends Component {
     render() {
         return (
             <Navbar>
-                <NavItem icon= "😃"/>
-                <NavItem icon= "😃"/>
-                <NavItem icon= "😃"/>
+                <NavItem icon={<BellIcon />} />
+                <NavItem icon={<ProfileIcon />}>
+                    {/*Dropdown goes here*/}
+                    <DropdownMenu />
+                </NavItem>
 
             </Navbar>
-        )
+        );
     }
 }
 
+function DropdownMenu(){//dropdownmenu function
+
+    function DropdownItem(props){//Items nested in the function
+        return(
+            <a href="#" className="menu-item">
+                <span className="icon-button">{props.leftIcon}</span>{/*Icon on the left*/}
+                {props.children}
+
+                <span className="icon-right">{props.rightIcon}</span>{/*Icon on the right*/}
+                
+            </a>
+        );
+    }
+
+
+    return(
+        <div className="dropdown">
+            {/*adds an item called My Profile*/}
+            <DropdownItem 
+              leftIcon = {<ProfileIcon/>}
+              rightIcon={<RightChevron/>}>
+                  My Profile
+            </DropdownItem>
+
+            {/*adds an item called Settings*/}
+            <DropdownItem
+              leftIcon={<CogIcon />}
+              rightIcon={<RightChevron/>}>
+                  Settings
+            </DropdownItem>
+
+            {/*adds an item called Log Out*/}
+            <DropdownItem
+              leftIcon={<PowerIcon />}
+              >
+                  Log Out
+            </DropdownItem>
+        </div>
+    );
+}
+
+
 function Navbar(props){ //function of the navbar which is the topbar
+
     return(
         <nav className="topbar">
             <ul className="topbar-nav">{props.children}</ul>
@@ -27,11 +79,20 @@ function Navbar(props){ //function of the navbar which is the topbar
 }
 
 function NavItem(props){//function for the items within the navbar
+
+    //returns two values in an array
+    //first value is state called open, boolean which tells us if menu is open
+    //second value is function that changes state
+    const [open, setOpen] = useState(false); //closed by default, so parameter set to false
+
+
     return(
         <li className="nav-item">
-            <a href="#" className = "icon-button">
+            <a href="#" className = "icon-button" onClick={() => setOpen(!open)}>{/*changes state on a click, toggles the boolean*/}
                 {props.icon}
             </a>
+
+            {open && props.children} {/* if open is true then it will show the children*/ }
         </li>
     );
 }
