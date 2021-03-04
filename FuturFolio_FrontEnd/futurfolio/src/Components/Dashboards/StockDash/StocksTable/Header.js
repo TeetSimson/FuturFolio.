9f960c5acf083ef5405e9165c47883a1b69acb1d
Teet Simson <teetsimson@Teets-MBP.lan>
2021-03-04 17:24:27 +0200

    New Table

---
+++ FuturFolio_FrontEnd/futurfolio/src/Components/Dashboards/StockDash/StocksTable/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Header.css';
+
+export default function Header() {
+    return (
+                <tr className="MainRow">
+                  <td id="Reset"></td>
+                  <td id="Reset">Ticker</td>
+                  <td id="Reset">Name</td>
+                  <td id="Reset">Price</td>
+                  <td id="Reset">Gain/Loss</td>
+                  <td id="Reset">1Y Low</td>
+                  <td id="Reset">1Y Graph</td>
+                  <td id="Reset">Dividend</td>
+                  <td id="Reset">Ex. Dividend Date </td>
+                  <td id="Reset">DCR</td>
+                  <td id="Reset">Amount</td>
+                  <td id="Reset">Avrg. Price </td>
+                  <td id="Reset">Invested</td>
+                  <td id="Reset">Value</td>
+                  <td id="Reset">Change</td>
+                  <td id="Reset">%</td>
+                  <td id="Reset">Dividends</td>
+                </tr>
+    )
+}
