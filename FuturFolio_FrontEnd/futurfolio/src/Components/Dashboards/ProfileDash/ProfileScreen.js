0d7a443fe7ba649969be519ad53123d057528614
a58946lt <lu.tian-2@student.manchester.ac.uk>
2021-03-10 21:21:55 +0000

    Made changes to profile page

--- FuturFolio_FrontEnd/futurfolio/src/Components/Dashboards/ProfileDash/ProfileScreen.js
+++ FuturFolio_FrontEnd/futurfolio/src/Components/Dashboards/ProfileDash/ProfileScreen.js
@@ -1,30 +1,120 @@
-import React, { Component } from 'react';
-import profilepic from './profilepic.jpg';
+import React, { Component} from 'react';
 import "./ProfileScreen.css"
 import { ReactComponent as ProfileIcon } from './Icons/profileIcon.svg';
+import { ReactComponent as CogIcon } from './Icons/cogIcon.svg';
+
+
+
+
+
 
 export default class ProfileScreen extends Component{
+    Settings(props){
+        function Profsetbutton(props){//settings button
+            return(
+                <div className="profsetitem" onClick={props.DashChangeSettings}>
+                    <a href="#" className = "profsetbutton" >
+                    <span className="leftIcon">{props.leftIcon}</span>{/*Icon on the left*/}
+                        {props.children}
+                    </a>
+                </div>
+            );
+        }
+        return(
+                <div>
+                    <Profsetbutton 
+                        leftIcon={<CogIcon />}
+                        >
+                        Profile Settings
+                    </Profsetbutton>
+                </div>
+        );
+    }
+    
+    
+    nameInfo(){
+        const name = "Elon Musk";
+        return(
+            <div className = "Name"><h1>{name}</h1></div>  
+        );
+    }
+
+    description(){
+        const descripvar= "Hello there I am using Futurfolio for my finances";
+        return(
+                <div className = "Description">
+                    <h3>{descripvar}</h3>
+                </div>
+        );
+    }
+
+   email(){
+        const emailvar= "email@mail.com";
+        return(
+                <div className = "Email">
+                    <h2>Email:</h2>
+                    <h3>{emailvar}</h3>
+                </div>
+        );
+    }
+
+    location(){
+        const locvar= "United Kingdom";
+        return(
+                <div className = "Location">
+                    <h2>Location:</h2>
+                    <h3>{locvar}</h3>
+                </div>
+        );
+    }
+
+    friends(){
+        const friendvar= "0";
+        return(
+                <div className = "Friends">
+                    <h2>Friends:</h2>
+                    <h3>{friendvar}</h3>
+                </div>
+        );
+    }
+
+
     render(){
         return(
-            <div>
                 <div>
-                    <div className="ProfilePos">
-                        <div className="ProfileIcon">
-                            <ProfileIcon/>
-                        </div>
-                        
-                    </div>
                     <div>
-                        <h4>Name holder</h4>
-                        <div style={{diplay:"flex", justifyContent:"space-between", width:"108%"}}>
-                            <h6>Location</h6>
-                            <h6>Age</h6>
-                            <h6>Friends</h6>
+                        <div className="ProfilePos">
+                            <div className="ProfileIcon">
+                                <ProfileIcon/>
+                            </div>
+                            <div>
+                                <this.nameInfo/>
+                                <this.description/>
+                                <this.Settings
+                                DashChangeSettings={this.props.DashChangeSettings}
+                                />
+                                
+                            </div>
+                            <div>
 
+                            </div>
+                        </div>
+                    </div>
+                   
+                    <div>
+                        <div class="Row">
+                            <div class ="Column"><this.email/></div>
+                            <div class ="Column"><this.location/></div>
+                            <div class ="Column"><this.friends/></div>
+                        </div>
+                        <div>
+                            <div class="Acheivements">
+                                <h1>Acheivements</h1>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        )
+                    
+        );
     }
 }
