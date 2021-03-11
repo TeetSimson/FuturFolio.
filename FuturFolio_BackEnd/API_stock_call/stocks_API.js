0dc49d1117460f3336f871ee394f2ecd1b921591
a47528ci <codrut-grigore.irimie@student.manchester.ac.uk>
2021-03-11 11:53:54 +0000

    fetch all stock data

---
+++ FuturFolio_BackEnd/API_stock_call/stocks_API.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = require("express").Router();
+const fetch = require('node-fetch');
+
+const app = express();
+
+const API_KEY="NNL94ZONNPS8XSPS";
+const stockName = "IBM";
+let API_call=`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${stockName}&outputsize=full&apikey=${API_KEY}`;
+
+fetch(API_call)
+    .then(res => res.json())
+    .then(text => {
+      router.post('/allData', (req, res) => {
+        res.json(text);
+      });
+    })
+
+module.exports = router;
