afffe7e76d441e625f45ac63a1f913fe50265f41
a47528ci <codrut-grigore.irimie@student.manchester.ac.uk>
2021-03-16 22:37:06 +0000

    stock update v0.2

--- FuturFolio_BackEnd/API_stock_call/Mstocks_API.js
+++ FuturFolio_BackEnd/API_stock_call/Mstocks_API.js
@@ -4,6 +4,7 @@ const fetch = require('node-fetch');
 
 
 var stockName;
+var data;
 var API_call;
 const API_KEY="NNL94ZONNPS8XSPS";
 
@@ -12,12 +13,28 @@ router.post('/allData/:symbol', (req, res) => {
   stockName = req.params.symbol;
   API_call=`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${stockName}&outputsize=full&apikey=${API_KEY}`;
 
+
   fetch(API_call)
-      .then(res => res.json())
-      .then(text => {
+      .then(res =>
+        //console.log(res);
+        res.json()
+      )
+      .then(text =>{
+        //console.log("==============\n");
+        //console.log(res);
           res.json(text);
+          data = text;
+          console.log(data);
         });
 
+  //res.json(text);
+/*
+  fetch(API_call)
+        .then(res => res.json())
+        .then(text => {
+            res.json(text);
+          });
+*/
 })
 
 
--- FuturFolio_BackEnd/API_stock_call/stocks_update.js
+++ FuturFolio_BackEnd/API_stock_call/stocks_update.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 */
 
-var stockName;
+var StockName;
 var API_call;
 const API_KEY="NNL94ZONNPS8XSPS";
 
@@ -18,18 +18,34 @@ router.post('/updateJSON', async (req, res) => {
   console.log(req.body.stocks);
   let stocks = req.body.stocks;
 
-  async function update(stocks, mergedJSON)
+  async function getJSON(StockName)
   {
+    API_call=`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${StockName}&outputsize=full&apikey=${API_KEY}`;
+
+    return fetch(API_call)
+      .then(res => res.json())
+      .then(text => {
+            return text;
+          });
+  }
+
+
+
+
+
+  async function update(stocks)
+  {
+    mergedJSON = [];
     // var JSONsList = stocks;
     // JSONsList = JSON.parse(JSONsList);
     for (var item of stocks)
     {
-      console.log(item.stockName + "===========");
+
       StockName = item.stockName;
-      API_call=`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${StockName}&outputsize=full&apikey=${API_KEY}`;
+      //API_call=`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${StockName}&outputsize=full&apikey=${API_KEY}`;
 
       try{
-        var response = await fetch(API_call);
+        var response = await getJSON(StockName);
         //jsonBlock = await response.text().json();
         /*
         fetch(API_call)
@@ -38,16 +54,19 @@ router.post('/updateJSON', async (req, res) => {
                 res.json(text);
               });
         */
-        mergedJSON.append(res);
+        mergedJSON.push(response);
+        console.log(mergedJSON);
       } catch (e){
         console.log('Ohh oh');
         console.error(e);
       }
     }
     //res.json(mergedJSON);
+    return mergedJSON;
   }
 
-  res.json(update(stocks, mergedJSON));
+  res.send(await update(stocks));
+  //res.send(await getJSON("IBM"));
 })
 
 module.exports = router;
