94625f24ae138800ddb7214e01b644f772843a7c
a88472rk <ryan.karibwami@student.manchester.ac.uk>
2021-04-14 20:34:05 +0100

    added change password, change name and close account routes

--- FuturFolio_BackEnd/routers/userDetailsRouter.js
+++ FuturFolio_BackEnd/routers/userDetailsRouter.js
@@ -3,6 +3,7 @@ const User = require("../models/userModel");
 const auth = require("../middleware/auth");
 const multer = require("multer");
 const upload = multer();
+const bcrypt = require("bcryptjs");
 
 router.post("/profileImage",upload.array("image","token"), auth, async (req,res) =>{
 	try{
@@ -55,6 +56,76 @@ router.post("/profileImageRetrieve", auth, async (req,res) =>{
 
 
 	}catch (err){
+		console.error(err);
+		res.status(500).send();
+	}
+});
+
+router.post("/changeName",auth,async(req,res)=>{
+	try{
+
+		const id = req.user;
+		const {name} = req.body;
+
+		if(!name)
+			return res.status(400).json({errorMessage:"Please enter a name."})
+
+		await User.findByIdAndUpdate(
+			id,
+			{$set:{name:name}})
+
+	}catch (err){
+		console.error(err)
+		res.status(500).send
+	}
+})
+
+router.post("/changePassword", auth, async (req,res) =>{
+	try{
+		
+		const id = req.user;
+		const {currentPassword,newPassword,newPasswordVerify} = req.body;
+
+		if(!currentPassword||!newPassword||!newPasswordVerify)
+			return res.status(400).json({errorMessage:"Please fill all required fields."});
+
+		if(newPassword!=newPasswordVerify)
+			return res.status(400).json({errorMessage:"New passwords do not match."});
+
+		userDetails = await User.findById(id);
+
+
+		bcrypt.compare(currentPassword, userDetails.passwordHash)
+		.then(passwordCorrect => {
+			if (!passwordCorrect)
+				return res.status(401).json({errorMessage: "Please enter a valid current password."});
+		});
+
+		bcrypt.compare(newPassword, userDetails.passwordHash)
+		.then(passwordCorrect => {
+			if (passwordCorrect)
+				return res.status(401).json({errorMessage: "New password cannot be the same as old password."});
+		});
+
+		const salt = await bcrypt.genSalt();
+		const newPasswordHash = await bcrypt.hash(newPassword, salt);
+
+		await User.findByIdAndUpdate(
+			id,
+			{$set:{passwordHash:newPasswordHash}});
+
+
+	}catch (err){
+		console.error(err);
+		res.status(500).send();
+	}
+});
+
+router.post("/closeAccount", auth, async (req,res) =>{
+	try{
+		id = req.user;
+		await User.findByIdAndRemove(id);
+	}catch(err){
 		console.error(err);
 		res.status(500).send();
 	}
